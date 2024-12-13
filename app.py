f5ab1145f642d6d18884d244ffb576f76fb45253
noorlight007 <contactwnoor@gmail.com>
2024-12-13 20:08:28 +0600

    update

--- app.py
+++ app.py
@@ -232,4 +232,4 @@ def close_db_connection(exception=None):
         print(f"Error closing database connection: {e}")
 
 if __name__ == '__main__':
-    app.run(debug=True,host = '0.0.0.0', port= 8000)
+    app.run(debug=True, host = '0.0.0.0', port= 8000)
